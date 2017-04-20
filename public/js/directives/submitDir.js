9e20efa5a3b45bb0f4f74a2d56486bef22ec10d0
Timothy Daniel Mitchell <m.timothyd@gmail.com>
2017-04-19 18:22:59 -0600

    623pm

--- public/js/directives/submitDir.js
+++ public/js/directives/submitDir.js
@@ -5,14 +5,21 @@ angular.module("placid").directive("submitDir",[function(){
     scope:{},
     controller:["$scope","placidService",function($scope,placidService){
       $scope.determination = ""
+      $scope.isHidden = false;
       $scope.testPost = () => {
         $scope.determination = "robots are reading your post"
         placidService.testPost($scope.post).then((result)=>{
           console.log(result);
-          if(typeof result.data === "string")$scope.determination = result.data;
+          if(typeof result.data === "string"){
+            $scope.determination = result.data
+            $scope.isHidden = true
+            for (let prop in $scope.post){
+              $scope.post[prop] = ""
+            }
+          }
           else {
-            $scope.determination = "Robots want you to address the following:";
-
+            $scope.determination = "Robots want you to address the following:"
+            $scope.fixes = result.data
           }
         })
       };
