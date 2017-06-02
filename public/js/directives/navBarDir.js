d7225ec45d85ee682d62aa6eda0a16e320d00be6
Timothy Daniel Mitchell <m.timothyd@gmail.com>
2017-06-02 13:04:36 -0600

    better navbar

--- public/js/directives/navBarDir.js
+++ public/js/directives/navBarDir.js
@@ -3,9 +3,9 @@ angular.module("placid").directive("navBar",[function(){
   return{
     template:`<link rel="stylesheet" href="../styles/navBarDir.css"/>
               <div class="navBar">
-              <a ui-sref="about"><button type="button" class="nav">About</button></a>
-              <a ui-sref="posts"><button type="button" class="nav">Posts</button></a>
-              <a ui-sref="submit"><button type="button" class="nav">Create Post</button></a>
+              <a ui-sref="about"><div class="nav"><div>About</div></div></a>
+              <a ui-sref="posts"><div class="nav"><div>Posts</div></div></a>
+              <a ui-sref="submit"><div class="nav"><div>Create Post</div></div></a>
               <a href="https://github.com/timothydm42/personalProject" target="_blank"
                 style="position:fixed; top:3px; right:18px;"><img src="../../github.png" style="height:50px; width:50px;"/></a>
               </div>`,
